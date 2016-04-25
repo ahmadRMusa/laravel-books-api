4c0c94d7448aae78fdc352e656cebd39566a83ac
Daniel C <bitclaw@gmail.com>
2016-04-25 08:13:31 -0500

    Add invoices unit test.

---
+++ tests/InvoicesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Class BooksTest
+ */
+class InvoicesTest extends TestCase {
+
+    /** @test */
+    public function testfetchInvoices() {
+        $this->json('GET', '/api/v1/invoices')
+            ->seeJsonStructure([
+                '*' => [
+                    'id', 'amount','created_at','quantity','updated_at'
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function testFetchSingleInvoice() {
+        $this->get('/api/v1/invoices/1')
+            ->seeJsonStructure([
+                'id',
+                'amount',
+                'quantity',
+                'updated_at'
+            ]);
+    }
+}
